4d2442ed5b260d7cb6fd1c65058f892df538ae4e
Mihai-Ciprian Chezan <mikache82@gmail.com>
2024-06-21 17:44:52 -0400

    Corrected flake warnings

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -72,12 +73,12 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<id>", methods=["GET"])
-def read_account(id:int):
+def read_account(id):
     """
     Reads an Account
     This endpoint will return the account data corresponding to the provided id
@@ -88,7 +89,7 @@ def read_account(id:int):
     if not found_account:
         abort(status.HTTP_404_NOT_FOUND , f"Account with id: {id} was not found")
     return found_account.serialize(), status.HTTP_200_OK
-    
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -127,8 +128,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
